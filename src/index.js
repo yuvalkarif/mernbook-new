f90d770a1dcdc1fd49a29aff1d4e51a2bbdedd20
Yuval Karif <34835592+yuvalkarif@users.noreply.github.com>
2021-10-24 14:21:03 +0300

    public location

--- src/index.js
+++ src/index.js
@@ -43,9 +43,9 @@ passportConfig(passport);
 
 //Routing
 if (process.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname, "/../../client/build")));
+  app.use(express.static(path.resolve(__dirname, "/../../client/public")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/../../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/../../client/public", "index.html"));
   });
 } else {
   app.use(express.static(path.resolve(__dirname, "../client/build")));
