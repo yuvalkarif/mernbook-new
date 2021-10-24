6803c2dc31eb0a2e44121f6aa05c9d8900724f66
Yuval Karif <34835592+yuvalkarif@users.noreply.github.com>
2021-10-24 15:31:24 +0300

    resolve => join

--- src/index.js
+++ src/index.js
@@ -43,9 +43,9 @@ passportConfig(passport);
 
 //Routing
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname + "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 } else {
   app.use(express.static(path.resolve(__dirname, "../client/build")));
